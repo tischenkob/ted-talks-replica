75288e4ff70b5c81948843ff2986fbb11b8fe092
Bogdan Tischenko <tischenkob@gmail.com>
2021-04-23 04:31:25 +0300

    Enable repositories

--- src/main/kotlin/ru/ifmo/ted/TedApplication.kt
+++ src/main/kotlin/ru/ifmo/ted/TedApplication.kt
@@ -2,8 +2,10 @@ package ru.ifmo.ted
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.data.jdbc.repository.config.EnableJdbcRepositories
 
 @SpringBootApplication
+@EnableJdbcRepositories("ru.ifmo.ted.repository")
 class TedApplication
 
 fun main(args: Array<String>) {
