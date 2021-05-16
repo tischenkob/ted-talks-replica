b83ed56ac0e86a54f52e2a0f989944b5ad5f9fdb
Bogdan Tischenko <tischenkob@gmail.com>
2021-05-16 19:43:31 +0300

    Rename Event to Talk

--- src/main/kotlin/ru/ifmo/ted/controller/TalkController.kt
+++ src/main/kotlin/ru/ifmo/ted/controller/TalkController.kt
@@ -1,25 +1,25 @@
 package ru.ifmo.ted.controller
 
 import org.springframework.web.bind.annotation.*
-import ru.ifmo.ted.model.Event
+import ru.ifmo.ted.model.Talk
 import ru.ifmo.ted.model.Request
-import ru.ifmo.ted.service.EventService
+import ru.ifmo.ted.service.TalkService
 import java.security.Principal
 
 @RestController
 @RequestMapping("/api/events")
-class EventController(
-    val eventService: EventService
+class TalkController(
+    val talkService: TalkService
 ) {
 
     @GetMapping
-    fun getAllEvents(): MutableIterable<Event> {
-        return eventService.getAllEvents()
+    fun getAllEvents(): MutableIterable<Talk> {
+        return talkService.getAllTalks()
     }
 
     @GetMapping("{id}/requests")
     fun getRequestsForEventWith(@PathVariable id: Long): Set<Request> {
-        return eventService.getRequestsForEventWith(id)
+        return talkService.getRequestsForEventWith(id)
     }
 
     @PostMapping("{id}/requests/{rid}")
@@ -28,12 +28,12 @@ class EventController(
         @PathVariable("rid") requestId: Long,
         @RequestBody state: String
     ) {
-        eventService.updateRequestWithState(id, requestId, state)
+        talkService.updateRequestWithState(id, requestId, state)
     }
 
     @GetMapping("{id}/attend")
     fun postRequestForEventWith(@PathVariable id: Long, principal: Principal) {
-        eventService.postRequestForEventWith(id, principal)
+        talkService.postRequestForTalkWith(id, principal)
     }
 
 }
