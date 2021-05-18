cfdc6c8dd446179f7102f9d74adc92f54c5b54d4
Bogdan Tischenko <tischenkob@gmail.com>
2021-05-18 16:21:32 +0300

    remove schema name from queries

--- src/main/kotlin/ru/ifmo/ted/repository/Repositories.kt
+++ src/main/kotlin/ru/ifmo/ted/repository/Repositories.kt
@@ -9,24 +9,24 @@ import ru.ifmo.ted.model.Talk
 import ru.ifmo.ted.model.Request
 
 interface PersonRepository : CrudRepository<Person, Long> {
-    @Query("SELECT p.* FROM ted.person p WHERE username = :uname")
+    @Query("SELECT p.* FROM person p WHERE username = :uname")
     fun findByUsername(@Param("uname") value: String): Person?
 
-    @Query("SELECT * FROM ted.person")
+    @Query("SELECT * FROM person")
     override fun findAll(): Set<Person>
 
-    @Query("SELECT p.* FROM ted.request r JOIN ted.person p ON r.person = p.id WHERE r.id = :value")
+    @Query("SELECT p.* FROM request r JOIN person p ON r.person = p.id WHERE r.id = :value")
     fun findByRequestId(@Param("value") value: Long): Person
 }
 
 interface TalkRepository : CrudRepository<Talk, Long>
 
 interface RequestRepository : CrudRepository<Request, Long> {
-    @Query("SELECT * FROM ted.request WHERE event = :event_id")
+    @Query("SELECT * FROM request WHERE talk = :event_id")
     fun findByAllEventId(@Param("event_id") value: Long): Set<Request>
 }
 
 interface NotificationRepository : CrudRepository<Notification, Long> {
-    @Query("SELECT * FROM ted.person p WHERE id = :id")
+    @Query("SELECT * FROM person p WHERE id = :id")
     fun findAllByPersonId(@Param("id") value: Long): Set<Notification>
 }
