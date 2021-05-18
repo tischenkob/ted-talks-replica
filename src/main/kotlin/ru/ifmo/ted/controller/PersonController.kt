94baac90aa9b6fd1a38ab49718416db3e3956782
Bogdan Tischenko <tischenkob@gmail.com>
2021-05-18 16:20:38 +0300

    Extract getNotifications to NotificationService

--- src/main/kotlin/ru/ifmo/ted/controller/PersonController.kt
+++ src/main/kotlin/ru/ifmo/ted/controller/PersonController.kt
@@ -1,23 +1,12 @@
 package ru.ifmo.ted.controller
 
-import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
-import ru.ifmo.ted.model.Notification
-import ru.ifmo.ted.model.Person
-import ru.ifmo.ted.repository.NotificationRepository
-import ru.ifmo.ted.repository.PersonRepository
 import ru.ifmo.ted.service.PersonService
-import java.security.Principal
 
 
 @RestController
 @RequestMapping("/api/people")
 class PersonController(val personService: PersonService) {
-
-    @GetMapping("/notifications")
-    fun getNotifications(principal: Principal): Set<Notification> {
-        return personService.getNotifications(principal)
-    }
 
 }
