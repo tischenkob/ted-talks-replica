29247d0e0d451d4150e368ecc84affbd8848ce2a
Bogdan Tischenko <tischenkob@gmail.com>
2021-05-21 08:31:08 +0300

    Add @EnableTransactionManagement

--- src/main/kotlin/ru/ifmo/ted/TedApplication.kt
+++ src/main/kotlin/ru/ifmo/ted/TedApplication.kt
@@ -5,16 +5,18 @@ import org.springframework.boot.builder.SpringApplicationBuilder
 import org.springframework.boot.runApplication
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
 import org.springframework.data.jdbc.repository.config.EnableJdbcRepositories
+import org.springframework.transaction.annotation.EnableTransactionManagement
 
 
 @SpringBootApplication
 @EnableJdbcRepositories("ru.ifmo.ted.repository")
-class TedApplication: SpringBootServletInitializer() {
-	override fun configure(builder: SpringApplicationBuilder): SpringApplicationBuilder? {
-		return builder.sources(TedApplication::class.java)
-	}
+@EnableTransactionManagement
+class TedApplication : SpringBootServletInitializer() {
+    override fun configure(builder: SpringApplicationBuilder): SpringApplicationBuilder? {
+        return builder.sources(TedApplication::class.java)
+    }
 }
 
 fun main(args: Array<String>) {
-	runApplication<TedApplication>(*args)
+    runApplication<TedApplication>(*args)
 }
