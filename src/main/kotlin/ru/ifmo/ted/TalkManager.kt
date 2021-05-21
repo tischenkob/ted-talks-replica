cc08e52f23558052cf90fe814dff17b377665db7
Bogdan Tischenko <tischenkob@gmail.com>
2021-05-21 08:30:26 +0300

    Move @Transactional to upper level caller and move talkService.save(talk) below

--- src/main/kotlin/ru/ifmo/ted/TalkManager.kt
+++ src/main/kotlin/ru/ifmo/ted/TalkManager.kt
@@ -3,7 +3,6 @@ package ru.ifmo.ted
 import org.springframework.context.ApplicationEvent
 import org.springframework.context.ApplicationEventPublisher
 import org.springframework.stereotype.Service
-import org.springframework.transaction.annotation.Propagation
 import org.springframework.transaction.annotation.Transactional
 import org.springframework.transaction.event.TransactionPhase
 import org.springframework.transaction.event.TransactionalEventListener
@@ -28,6 +27,7 @@ class TalkManager(
     class RequestStateChangedEvent(source: Any, val person: Person, val title: String, val state: String) :
         ApplicationEvent(source)
 
+    @Transactional
     fun findRequestByIdAndUpdateWithState(talkId: Long, requestId: Long, state: String) {
         val talk = talkService.findById(talkId)
         val request = talk.getRequestWithId(requestId)
@@ -35,12 +35,11 @@ class TalkManager(
         changeRequestState(person, talk, request, state)
     }
 
-    @Transactional(propagation = Propagation.REQUIRES_NEW, transactionManager = "transactionManager")
     fun changeRequestState(person: Person, talk: Talk, request: Request, state: String) {
         val oldState = request.state
         val newState = requestService.changeRequestState(request, state)
-        talkService.save(talk)
         talkService.changeSpeakerCounterValue(talk, oldState, newState)
+        talkService.save(talk)
 
         val event = RequestStateChangedEvent(this, person, talk.title, state)
         applicationEventPublisher.publishEvent(event)
